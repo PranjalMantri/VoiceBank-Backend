922b44adcdbfcee3a86d0c76379c96edd7af9426
PranjalMantri <pranjalmantri@gmail.com>
2024-02-18 21:41:32 +0530

    added utils

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
-import mongoose, { connect } from "mongoose";
+import app from "./app.js";
 import connectDB from "./db/index.js";
 import dotenv from "dotenv";
 dotenv.config();
 
-connectDB();
+const port = process.env.PORT || 8000;
+
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("Error ", error);
+    });
+
+    app.listen(port, () => {});
+  })
+  .catch((err) => {
+    console.log("MongoDB connection Error: ", err);
+  });
