0ad8e3d72387200871f62fdd33c917501eea2091
PranjalMantri <pranjalmantri@gmail.com>
2024-02-19 01:34:05 +0530

    created transaction routes

--- src/app.js
+++ src/app.js
@@ -23,7 +23,10 @@ app.use(cookieParser());
 // importing routers
 import userRouter from "./routes/user.routes.js";
 import accountRouter from "./routes/accout.routes.js";
+import transactionRouter from "./routes/transaction.routes.js";
 
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/account", accountRouter);
+app.use("/api/v1/transaction", transactionRouter);
+
 export default app;
