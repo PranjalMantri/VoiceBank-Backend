d207f70d72f67e142cca3044c9464e1e6f15b8ab
PranjalMantri <pranjalmantri@gmail.com>
2024-02-19 01:39:22 +0530

    added transaction model

--- src/models/transaction.model.js
+++ src/models/transaction.model.js
@@ -1,1 +1,25 @@
-ECHO is on.
+import mongoose from "mongoose";
+
+const transactionSchema = new mongoose.Schema(
+  {
+    type: {
+      type: String,
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+    fromAccount: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Account",
+    },
+    toAccount: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Account",
+    },
+  },
+  { timestamps: true }
+);
+
+export const Transaction = mongoose.model("Transaction", transactionSchema);
