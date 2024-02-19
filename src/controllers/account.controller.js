042d6063958384aa913b948e18305889a5228955
PranjalMantri <pranjalmantri@gmail.com>
2024-02-19 15:41:52 +0530

    Added getAccountOwner functionality

--- src/controllers/account.controller.js
+++ src/controllers/account.controller.js
@@ -3,19 +3,114 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/apiError.js";
 import { ApiResponse } from "../utils/apiResponse.js";
 import { isValidObjectId } from "mongoose";
+import { User } from "../models/user.model.js";
 import aleaRNGFactory from "number-generator/lib/aleaRNGFactory.js";
+import mongoose from "mongoose";
 
 const createAccount = asyncHandler(async (req, res) => {
-  //TODO: Create user's account
-  // Testing for account number using external library
+  const { accountType } = req.body;
 
-  const { uInt32 } = aleaRNGFactory(5);
-  const number = uInt32();
-  console.log(number);
+  if (!accountType) {
+    throw new ApiError(404, "Invalid account type");
+  }
+
+  const alreadyHasAccount = await Account.findOne({
+    owner: req.user._id,
+    accountType,
+  });
+
+  if (alreadyHasAccount) {
+    throw new ApiError(500, `User already has a ${accountType} account`);
+  }
+
+  const accountNumber = aleaRNGFactory(req.user.customerId).uInt32();
+
+  if (!accountNumber) {
+    throw new ApiError(
+      500,
+      "Something went wrong while generating account number"
+    );
+  }
+
+  const account = await Account.create({
+    owner: req.user._id,
+    accountType,
+    accountNumber,
+  });
+
+  if (!account) {
+    throw new ApiError(500, "Something went wrong while creating account");
+  }
+
+  res.status(200).json(new ApiResponse(200, account, "Created User account"));
 });
 
 const getAccount = asyncHandler(async (req, res) => {
-  //TODO: get account by accountId
+  const { accountId } = req.params;
+
+  if (!isValidObjectId(accountId)) {
+    throw new ApiError(404, "Invalid account Id");
+  }
+
+  const account = await Account.findById(accountId);
+
+  if (!account) {
+    throw new ApiError(404, "Account does not exist");
+  }
+
+  res
+    .status(200)
+    .json(new ApiResponse(200, account, "Successfuly fetched account by id"));
+});
+
+const getAccountOwner = asyncHandler(async (req, res) => {
+  const { accountId } = req.params;
+
+  if (!isValidObjectId(accountId)) {
+    throw new ApiError(404, "Invalid account Id");
+  }
+
+  const account = await Account.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(accountId),
+      },
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "owner",
+        foreignField: "_id",
+        as: "owner",
+        pipeline: [
+          {
+            $project: {
+              _id: 0,
+              username: 1,
+              customerId: 1,
+            },
+          },
+        ],
+      },
+    },
+    {
+      $project: {
+        username: { $arrayElemAt: ["$owner.username", 0] },
+        customerId: { $arrayElemAt: ["$owner.customerId", 0] },
+        _id: 0,
+      },
+    },
+  ]);
+
+  if (!account) {
+    throw new ApiError(404, "Account does not exist");
+  }
+
+  res
+    .status(200)
+    .json(
+      new ApiResponse(200, account, "Fetched account owner details successfuly")
+    );
 });
 
 const getUserBalance = asyncHandler(async (req, res) => {
@@ -26,4 +121,10 @@ const getUserTransactions = asyncHandler(async (req, res) => {
   //TODO: get user's transactions
 });
 
-export { createAccount, getAccount, getUserBalance, getUserTransactions };
+export {
+  createAccount,
+  getAccount,
+  getAccountOwner,
+  getUserBalance,
+  getUserTransactions,
+};
--- src/routes/accout.routes.js
+++ src/routes/accout.routes.js
@@ -1,11 +1,17 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { createAccount } from "../controllers/account.controller.js";
+import {
+  createAccount,
+  getAccount,
+  getAccountOwner,
+} from "../controllers/account.controller.js";
 
 const router = Router();
 
 router.use(verifyJWT);
 
 router.route("/create").post(createAccount);
+router.route("/:accountId").get(getAccount);
+router.route("/owner/:accountId").get(getAccountOwner);
 
 export default router;
